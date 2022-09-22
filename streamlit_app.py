b18bc339d3d2f31c487844d5144d7f8e14fcf9ae
Aashu1998 <71517513+Aashu1998@users.noreply.github.com>
2022-09-22 12:07:03 +0530

    Update streamlit_app.py
    Add few menu to app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
 
 
-streamlit.title("Hi Boss this is my new App")
+streamlit.title("My Parents New Healthy Diner")
+streamlit.header("Breakfast Menue")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard Boiled Free-Range Egg")
